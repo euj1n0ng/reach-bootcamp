a815ee174b77e197e98e07855d74b6eeba5155c3
Eujin Ong <62314480+euj1n0ng@users.noreply.github.com>
2022-04-03 19:39:56 +0700

    units-and-balances step 4

--- units-and-balances/index.mjs
+++ units-and-balances/index.mjs
@@ -1,4 +1,4 @@
-import {loadStdlib } from '@reach-sh/stdlib';
+import { loadStdlib } from '@reach-sh/stdlib';
 import * as backend from './build/index.main.mjs';
 
 const role = 'seller';
@@ -7,14 +7,10 @@ console.log(`Your role is ${role}.`);
 const stdlib = loadStdlib(process.env);
 console.log(`The consensus network is ${stdlib.connector}.`);
 const suStr = stdlib.standardUnit;
-const auStr = stdlib.atomicUnit;
 const toAU = (su) => stdlib.parseCurrency(su);
 const toSU = (au) => stdlib.formatCurrency(au, 4);
-const suBal = 1000;
-console.log(`Balance is ${suBal} ${suStr}`);
-const auBal = toAU(suBal);
-console.log(`Balance is ${auBal} ${auStr}`);
-console.log(`Balance is ${toSU(auBal)} ${suStr}`);
+const iBalance = toAU(1000);
+const showBalance = async (acc) => console.log(`Your balance is ${toSU(await stdlib.balanceOf(acc))} ${suStr}.`);
 
 (async () => {
   const commonInteract = {};
@@ -23,9 +19,11 @@ console.log(`Balance is ${toSU(auBal)} ${suStr}`);
   if (role === 'seller') {
     const sellerInteract = { ...commonInteract };
 
-    const acc = await stdlib.newTestAccount(stdlib.parseCurrency(1000));
+    const acc = await stdlib.newTestAccount(iBalance);
+    await showBalance(acc);
     const ctc = acc.contract(backend);
     await backend.Seller(ctc, sellerInteract);
+    await showBalance(acc);
   }
 
   // BUYER
